74959030f50ba151bcf13ddf53e9073c5b4c9124
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-05-22 10:53:19 +0330

    Update Webdrive (Chrome).py

--- Webdrive (Chrome).py
+++ Webdrive (Chrome).py
@@ -1,6 +1,5 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("http://www.google.com/")
 print(driver.title)
