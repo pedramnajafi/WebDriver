36904336a0e2119246508d97a89e2c5ff025bca7
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-05-22 10:53:35 +0330

    Update Webdrive (FireFox).py

--- Webdrive (FireFox).py
+++ Webdrive (FireFox).py
@@ -1,6 +1,5 @@
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium import webdriver
-
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 driver.get("http://www.google.com/")
 print(driver.title)
